080beba15badeed5c99d6e90f3d340b232034c67
Lorenzo Breda <lorenzo@lbreda.com>
2017-08-15 22:48:36 +0200

    Initial commit

---
+++ Instagram_image_downloader.meta.js
@@ -0,0 +1,5 @@
+// ==UserScript==
+// @version      1.0
+// @name         Instagram image downloader
+// @namespace    http://lbreda.com/
+// ==/UserScript==
---
+++ Instagram_image_downloader.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Instagram image downloader
+// @namespace    http://lbreda.com/
+// @version      1.0
+// @description  Adds a download link for instagram images in the one-image pages
+// @author       Lorenzo Breda
+// @match        https://*.instagram.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    if(document.querySelector('meta[property="og:image"]') && document.querySelector('meta[property="og:image"]').content){
+        var a = document.createElement('a');
+        var container = document.querySelector('._hmd6j._8oo9w');
+        a.href = document.querySelector('meta[property="og:image"]').content;
+        a.style.width = '24px';
+        a.style.height = '24px';
+        a.style.margin = '8px';
+        a.style.backgroundImage = "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAMAAADXqc3KAAAAV1BMVEUAAAAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAsPlAec59cAAAAHHRSTlMAAQUHCQoRGBkcHz9AUFZecYCVqLTK0dPp7ff5cWDvugAAAG1JREFUKFPFjUcOgDAMBE0vCb0n/v87IZZBCeQIYuTDakfyAnxAqRFHn5B48IcIN7xoHNNfvcodEZxGFeDC5tGz8fRkbn1HT5YoaDKoKK8pCZ6dE4Cas7AF6mE5o3QE0cbmPGJqzQlr3IbH32IHtA8TFl6BBTUAAAAASUVORK5CYII=')";
+        a.setAttribute('download','');
+        container.appendChild(a);
+    }
+})();
